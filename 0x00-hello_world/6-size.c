2583307ce72b891dee9aa17dba133464adf3b9b7
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-03 17:39:09 +0000

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float e;
-  printf("Size of a char: %zu bytes", sizeof(a));
-  printf("Size of a int: %zu bytes", sizeof(b));
-  printf("Size of a long int: %zu bytes", sizeof(c));
-  printf("Size of a long long int: %zu bytes", sizeof(d));
-  printf("Size of a float: %zu bytes", sizeof(e));
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %zu byte(s)", sizeof(a));
+printf("Size of a int: %zu byte(s)", sizeof(b));
+printf("Size of a long int: %zu byte(s)", sizeof(c));
+printf("Size of a long long int: %zu byte(s)", sizeof(d));
+printf("Size of a float: %zu byte(s)", sizeof(e));
 return (0);
 }
