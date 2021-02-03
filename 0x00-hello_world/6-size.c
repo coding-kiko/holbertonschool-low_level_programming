c16236341cd989ab9dbb75cbd1fdd5325d68b054
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-03 17:25:04 +0000

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char a;
+  int b;
+  long int c;
+  long long int d;
+  float e;
+  printf("Size of a char:", sizeof(a));
+  printf("Size of a char:", sizeof(b));
+  printf("Size of a char:", sizeof(c));
+  printf("Size of a char:", sizeof(d));
+  printf("Size of a char:", sizeof(e));  
+return (0);
+}
