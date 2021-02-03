e484b1dc9299a716d9e118ec82576f8094dff98f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-03 20:52:04 +0000

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 56);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
