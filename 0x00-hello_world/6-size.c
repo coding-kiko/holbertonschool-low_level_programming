cd2cfb43a7e755fb2bcffd958a3b7d0c836e9971
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-03 17:47:22 +0000

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 
 /**
  * main - Entry point
@@ -13,7 +13,7 @@ long int c;
 long long int d;
 float e;
 printf("Size of a char: %zu byte(s)\n", sizeof(a));
-printf("Size of a int: %zu byte(s)\n", sizeof(b));
+printf("Size of an int: %zu byte(s)\n", sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 printf("Size of a float: %zu byte(s)\n", sizeof(e));
