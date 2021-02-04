f30f27839abde39cb19f3b0817fd45e389aa26a5
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-04 19:10:10 +0000

    loops

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i < 57)
+putchar(44);
+if (i < 57)
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
