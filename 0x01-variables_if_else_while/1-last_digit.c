f7b51b34ac77e6a21df9b475e6e8e8c447c3c8a1
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-04 13:59:06 +0000

    loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@ int main(void)
 {
 int n;
 int a;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 a = (n % 10);
@@ -24,5 +25,6 @@ printf("Last digit of %d is %d and is 0\n", n, a);
 else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+}
 return (0);
 }
