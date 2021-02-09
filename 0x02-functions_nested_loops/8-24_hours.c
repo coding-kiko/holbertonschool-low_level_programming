3550a0a1bf0ab69ace5f92a02cf182dae6d072c5
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-09 17:34:11 +0000

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,32 @@
 #include "holberton.h"
 /**
- * print_last_digit - Entry point
- * @n: character
+ * jack_bauer - Entry point
  *
  * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
-  for 
+int a, b, c, d;
+for (a = '0'; a <= '2'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '5'; c++)
+{
+for (d = '0'; d <= '9'; d++)
+{
+_putchar(a);
+_putchar(b);
+_putchar(':');
+_putchar(c);
+_putchar(d);
+_putchar('\n');
+if (a == '2' && b == '3' && c == '5' && d == '9')
+goto end;
+}
+}
+}
+}
+end:
+;
 }
