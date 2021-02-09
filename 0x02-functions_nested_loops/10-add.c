aa22808054800f86b25c121a9eb04ae4ab24f05f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-09 13:36:48 +0000

    0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ * @x: number
+ * @y : numeber 1
+ * Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+int sum = x + y;
+return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Entry point
+ * @n: character
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+  for 
+}
