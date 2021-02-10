fbc393edd87bd045ebb6806233d6eddba15f1fdf
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-10 14:20:21 +0000

    0x02

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (succes)
+ */
+
+void main(void)
+{
+int a = 1;
+int sum = 0;
+for (a = 1; a < 1024; a++)
+{
+if ((a % 3 == 0) || (a % 5 == 0))
+sum = sum + a;
+}
+printf("%d", sum);
+}
