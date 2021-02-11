e17db9e49b1214292156c419c2d74aa04ed3e0fb
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-11 13:15:01 +0000

    0x04

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Entry point
+ * @x: number
+ * @y : numeber 1
+ * Return: Always 0 (Success)
+ */
+int mul(int x, int y)
+{
+	int mul = x * y;
+
+	return (mul);
+}
