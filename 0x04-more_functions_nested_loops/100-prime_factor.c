a29adafd75fa5fc754c0d57b0ee436fcd34c557e
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-11 18:58:30 +0000

    0x04

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,17 +12,15 @@ int main(void)
 	long int i = 612852475143;
 	long int num = i;
 	long int primo = 2;
-	long int max_prim = 2;
 
 	while (num != primo)
 	{
 		while (num % primo == 0)
 		{
 			num = num / primo;
-			max_prim = primo;
 		}
 		primo++;
 	}
-	printf("%ld", num);
+	printf("%ld\n", num);
 	return (0);
 }
