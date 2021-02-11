ebf4f5fbd96459cd720e0634cc8c687ca48d5f30
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-11 17:49:02 +0000

    0x04

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,9 +7,33 @@
  *
  */
 
-void main(void)
+int main(void)
 {
 	int i;
 
-	for ()
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (i % 5 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	putchar('\n');
+return (0);
 }
