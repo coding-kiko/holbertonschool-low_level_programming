d1671a892fa9cfdf615c3400f797e153e1454073
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-13 18:22:35 +0000

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - entry point
+ * @n: pointer to integer I want to change value
+ * Return: Nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
