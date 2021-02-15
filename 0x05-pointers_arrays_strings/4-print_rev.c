3699011b689cc6a6544df8296288e4c81ad15b54
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-15 20:15:01 +0000

    test w/out gcc

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -20,25 +20,3 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
-
-/**
- * _strlen - entry point
- * @s: string to find length
- * Return: length
- *
- */
-
-int _strlen(char *s)
-{
-	char a = s[0];
-	int i = 0;
-	int b;
-
-	while (a != 0)
-	{
-		a = s[i];
-		i++;
-	}
-	b = i - 1;
-	return (b);
-}
