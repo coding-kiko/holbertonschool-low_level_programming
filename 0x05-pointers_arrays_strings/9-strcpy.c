0184b5379673b57d36127b37d277fa8b3b4ed65f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-16 14:57:39 +0000

    0x05

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,10 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	char *p = dest;
 
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+	for (; *src != 0; src++, dest++)
+		*dest = *src;
+	*dest = '\0';
+	return (p);
 }
