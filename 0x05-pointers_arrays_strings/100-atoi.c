1ec0f73d70f1d287828b80472addd0a58326b935
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-17 14:07:27 +0000

    0x05

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,24 @@
 
 int _atoi(char *s)
 {
+	int c;
+	int n = 0;
 
+	for (c = 0; *s != 0; s++)
+	{
+		if (*s == '-')
+			c++;
+		if (*s >= 48 && *s <= 57)
+		{
+			n = (*s - '0') + (n * 10);
+			if (*(s + 1) < '0' || *(s + 1) > '9')
+			{
+				if (c % 2 == 0)
+					return (n);
+				else
+					return (-n);
+			}
+		}
+	}
+	return (n);
 }
