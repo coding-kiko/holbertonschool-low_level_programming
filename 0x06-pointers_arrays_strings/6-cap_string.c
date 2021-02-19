cf182154e0c8294fc09505107d2d85137223662d
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-19 19:23:51 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * cap_string - entry point
+ * @a: array to uppercase
+ * number of elements of the array
+ * Return: uppercase string
+ */
+
+char *cap_string(char *a)
+{
+	int i, j;
+	char s[] = {',', ';', '.', ' ', 10, '!', '?', '(', ')', '{', '}', '\t', '"'};
+
+	for (i = 0; a[i] != 0; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			for (j = 0; s[j] != '\0'; j++)
+			{
+				if (a[i - 1] == s[j])
+					a[i] = a[i] - 32;
+			}
+		}
+	}
+	return (a);
+}
