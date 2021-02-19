0bf1f1fcf6338e4bb552f121bdffda60d72053d8
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-19 19:32:43 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - entry point
+ * @s: string to find length
+ * Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
