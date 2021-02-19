b6d5f121b70c5aabfa6878bf9fc3a2351d41cbaf
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-19 14:26:51 +0000

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,25 @@
 #include "holberton.h"
 
 /**
+ * _strlen - entry point
+ * @s: string to find length
+ * Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
  * _strncpy - entry point
  * @dest: string to append
  * @src: string to be appended
@@ -12,8 +31,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *p = dest;
 	int i;
+	int l = _strlen(src);
 
 	for (i = 0; i < n; i++, src++, dest++)
-		*dest = *src;
-return (p);
+	{
+		if (i > l)
+			*dest = '\0';
+		else
+			*dest = *src;
+	}
+	return (p);
 }
