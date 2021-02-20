f8d15f3e260c6e07c31608680b497b44c38de67c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-20 00:25:33 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_number - entry point
+ * @n: number to be printed
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+
+	if ((num / 10) != 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
