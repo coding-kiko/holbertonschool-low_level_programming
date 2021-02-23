d9e958b598f7b6fc370ea174d9d72eb74e0288b6
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-23 16:37:27 +0000

    0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,30 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strcmp - entry point
- * @s1: string 1 to compare
- * @s2: string 2 to compare
- * Return: idk
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int a;
-	int b;
-
-	while (*s1 == *s2)
-	{
-		s1++;
-		s2++;
-		if (*s1 == '\0' || *s2 == '\0')
-			break;
-	}
-	a = *s1;
-	b = *s2;
-	return (a - b);
-}
-
-/**
  * _strchr - Entry point
  * @s: string to modify
  * @c: char to modify with
@@ -34,12 +10,12 @@ int _strcmp(char *s1, char *s2)
 
 char *_strchr(char *s, char c)
 {
+	if (c == '\0')
+		return ('\0');
 	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return (s);
 	}
-	if (c == '\0')
-		return ('\0');
 return ('\0');
 }
