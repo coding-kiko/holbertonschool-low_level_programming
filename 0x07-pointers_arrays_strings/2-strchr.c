5e4c445c672faa9ae0bb68e972ed3109239b43ef
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-23 16:32:42 +0000

    0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -34,12 +34,12 @@ int _strcmp(char *s1, char *s2)
 
 char *_strchr(char *s, char c)
 {
-	if (_strcmp("\0", s) == 0)
-		return ('\0');
 	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return (s);
 	}
+	if (c == '\0')
+		return ('\0');
 return ('\0');
 }
