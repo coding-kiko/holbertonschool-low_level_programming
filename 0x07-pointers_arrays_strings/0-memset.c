b9ed5d062f4aafd9f6d96ed35c110c6306513529
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-23 13:13:57 +0000

    0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memset - Entry point
+ * @s: string to modify
+ * @b: char to modify with
+ * @n: times the char replaces the memory
+ * Return: pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; s++, i++)
+		*s = b;
+	return (s);
+}
