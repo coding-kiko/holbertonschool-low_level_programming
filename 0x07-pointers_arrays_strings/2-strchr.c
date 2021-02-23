e55c0afcd30aedf8ac63e8a5cc943eb94530d460
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-23 14:04:13 +0000

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Entry point
+ * @s: string to modify
+ * @c: char to modify with
+ *
+ * Return: pointer to memory area s
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *p;
+
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			p = s;
+			break;
+		}
+	}
+	return (p);
+}
