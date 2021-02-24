e0d71a595e5ca2dbbaceec13459c9582ff707098
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-24 02:18:28 +0000

    0x07

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - Entry point
+ * @s: string to modify
+ * @to: char to modify with
+ *
+ * Return: pointer to memory area s
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
