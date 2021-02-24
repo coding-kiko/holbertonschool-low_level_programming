dd85b1175cd64a85511e2001e1f511ccd0207ca9
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-24 13:08:51 +0000

    0x08

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: string to modify
+ *
+ * Return: pointer to memory area s
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{	
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s++);
+}
