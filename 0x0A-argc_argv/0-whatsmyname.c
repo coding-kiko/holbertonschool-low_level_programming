c2ed60c4f29f101303f53da1378e3233c398d4a3
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-02-26 17:51:15 +0000

    0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of pointers pointing to these arguments
+ * Return: 0 succes
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
