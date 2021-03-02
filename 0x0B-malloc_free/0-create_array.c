f0cd32420fdc1bdc74e2cc954cf64dfb8be47ec4
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-02 17:21:26 +0000

    0x0B

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - entry point
+ * @size: number of arguments passed
+ * @c: array of pointers pointing to these arguments
+ * Return: 0 succes
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a = malloc(sizeof(char) * size);
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
