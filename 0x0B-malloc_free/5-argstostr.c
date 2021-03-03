39131ea0e460ddb62bbfd4ba5f278b3f3845ad9d
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-03 02:38:47 +0000

    0x0B

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argstostr - entry point
+ * @ac: number of arguments passed
+ * @av: array of pointers pointing to these arguments
+ * Return: 0 succes
+ */
+
+char *argstostr(int ac, char **av)
+{
+	return (av);
+}
