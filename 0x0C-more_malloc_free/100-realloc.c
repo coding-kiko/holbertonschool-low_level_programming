cf66c793cbf34bb9c12057e0bd1f137342477978
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-04 23:04:58 +0000

    0x0C

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -46,6 +46,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	else if (new_size == old_size)
+		return (ptr);
 	else if (!ptr)
 		return (malloc(new_size));
 	else if (new_size <= old_size)
