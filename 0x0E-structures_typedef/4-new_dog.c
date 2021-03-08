2e3013c884b9860fd2e7eccab7d946fcf6988a0c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-08 18:43:37 +0000

    0x0E

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: iwf
+ * jfjfjf
+ * Description: Longer description
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
