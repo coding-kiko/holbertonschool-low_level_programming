8d3cd1518b9df88a9fff122ead06e248c4bb1ad0
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-08 18:45:48 +0000

    0x0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,13 +6,14 @@
  * @name: First member
  * @age: Second member
  * @owner: iwf
- * jfjfjf
+ * Return: ptr
  * Description: Longer description
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
