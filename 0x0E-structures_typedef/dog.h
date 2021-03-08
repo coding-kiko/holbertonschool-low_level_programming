9856ce2b06e1fd866927eb18559579b8b783d2f9
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-08 17:03:07 +0000

    0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
 int _putchar(char c);
-typedef struct dog {
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 } dog;
+#endif
