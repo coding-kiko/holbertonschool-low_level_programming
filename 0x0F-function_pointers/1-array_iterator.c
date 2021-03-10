37f7e804d1f8ad0e8ff6b004113ee1ac2b41b650
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-10 13:41:23 +0000

    0x0F

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - Short description
+ * @array: First member
+ * @size: Second member
+ * @action: iwf
+ * jfjfjf
+ * Description: Longer description
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
