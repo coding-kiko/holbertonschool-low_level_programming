eec788c703936b9e8f2db2b995db557a33379529
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-10 13:03:58 +0000

    0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Short description
+ *  First member
+ *  Second member
+ * @name: iwf
+ * @f: jfjfjf
+ * Description: Longer description
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
