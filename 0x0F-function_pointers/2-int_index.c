b15d53401be57a5de4a2d08c304269597a4ba1fa
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-10 15:54:01 +0000

    0x0F

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * array_iterator - Short description
+ * int_index - Short description
  * @array: First member
  * @size: Second member
  * @cmp: iwf
@@ -13,12 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && size && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}		
 	return (-1);
 }
