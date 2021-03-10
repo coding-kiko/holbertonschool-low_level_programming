024532dec39542d9ab97387c268f5ddf1719db12
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-10 23:51:38 +0000

    3 sin comentarios todo junto

---
+++ 0x0F-function_pointers/pa_entender_lo_que_pasa_en_el_3.c
@@ -0,0 +1,81 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			break;
+		i++;
+	}
+	return (ops[i].f);
+}
+
+int main(int argc, char *argv[])
+{
+	int a, b, r;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	r = (*get_op_func(argv[2]))(a, b);
+	printf("%d\n", r);
+	return (0);
+}
