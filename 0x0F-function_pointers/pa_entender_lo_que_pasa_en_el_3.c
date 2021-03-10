21f3ce50ffc0b77a1dcd0f613f3faf95df1bbabc
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-10 23:54:56 +0000

    3 sin comentarios todo junto

--- 0x0F-function_pointers/pa_entender_lo_que_pasa_en_el_3.c
+++ 0x0F-function_pointers/pa_entender_lo_que_pasa_en_el_3.c
@@ -3,6 +3,25 @@
 #include <stdio.h>
 #include <strings.h>
 
+/*
+HEADER FILE:
+
+#ifndef CALC_H
+#define CALC_H
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
