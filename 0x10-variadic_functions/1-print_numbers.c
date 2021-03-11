be14b59d7c4609427588bfddc0c5ccb0e85f0a51
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-11 17:32:10 +0000

    0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(p, int));
-			if ((i != n) && *separator != '\0')
+			if ((i != n - 1) && separator != '\0')
 				printf("%s", separator);
 		}
 		va_end(p);
