0877acadb99ece62ea5bc424a05f8e396bca2bac
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-11 21:04:51 +0000

    0x10

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,19 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+void p_char(va_list p);
+void p_int(va_list p);
+void p_float(va_list p);
+void p_string(va_list p);
 #endif /* VARIADIC_FUNCTIONS_H */
