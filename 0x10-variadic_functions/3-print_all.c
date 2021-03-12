22deebbc38fbfda04316effe8473cb77681de945
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-12 01:43:38 +0000

    0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
  * print_numbers - Short description
@@ -10,42 +10,54 @@
  * Description: Sum numbers
  */
 
-void p_char(va_list p)
-{
-}
-void p_int(va_list p)
-{
-}
-void p_float(va_list p)
-{
-}
-void p_string(va_list p)
-{
-}
-
 void print_all(const char * const format, ...)
 {
-	int i, j;
+	int i = 0, j;
 	va_list p;
 
-	struct dt
-	{
-		op_t ops[] = {
-			{"c", print_char},
-			{"i", print_integer},
-			{"f", print_float},
-			{"s", print_string},
-			{'\0', '\0'}
-		};
-	}
+	op_t ops[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_string},
+		{'\0', '\0'}
+	};
+	va_start(p, format);
 	while (format[i])
 	{
-		while (format[i] == dt[j].o[0])
+		j = 0;
+		while (ops[j].op)
 		{
-			if (format)
+			if (format[i] == ops[j].op[0])
+			{
+				(ops[j].f(p));
+				printf(", ");
+			}
 			j++;
 		}
 		i++;
 	}
-		
+	printf("\n");
+	va_end(p);
+}
+
+void p_char(va_list p)
+{
+        printf("%c", va_arg(p, int));
+}
+void p_int(va_list p)
+{
+        printf("%i", va_arg(p, int));
+}
+void p_float(va_list p)
+{
+        printf("%f", va_arg(p, double));
+}
+void p_string(va_list p)
+{
+        char *str = va_arg(p, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
 }
