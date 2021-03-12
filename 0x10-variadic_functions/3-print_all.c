fcc3568429d04776ff0df45f87d91d6a4d024403
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-12 01:48:04 +0000

    0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - Short description
+ * print_all - Short description
  * @format: constant number n to be summe
  * Return: Sum of undefined quantity of numbers
  * Description: Sum numbers
@@ -41,23 +41,39 @@ void print_all(const char * const format, ...)
 	va_end(p);
 }
 
+/**
+ * p_char - Short description
+ * @p: constant number n to be summe
+ */
 void p_char(va_list p)
 {
-        printf("%c", va_arg(p, int));
+	printf("%c", va_arg(p, int));
 }
+/**
+ * p_int - Short description
+ * @p: constant number n to be summe
+ */
 void p_int(va_list p)
 {
-        printf("%i", va_arg(p, int));
+	printf("%i", va_arg(p, int));
 }
+/**
+ * p_float - Short description
+ * @p: constant number n to be summe
+ */
 void p_float(va_list p)
 {
-        printf("%f", va_arg(p, double));
+	printf("%f", va_arg(p, double));
 }
+/**
+ * p_string - Short description
+ * @p: constant number n to be summe
+ */
 void p_string(va_list p)
 {
-        char *str = va_arg(p, char *);
+	char *s = va_arg(p, char *);
 
-	if (str == NULL)
-		str = "(nil)";
-	printf("%s", str);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
 }
