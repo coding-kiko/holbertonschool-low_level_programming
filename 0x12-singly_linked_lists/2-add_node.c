ced3334de7de3642edf0203b4f4e3e17c6711416
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-18 23:07:06 +0000

    task 2 attached my own strlen function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,25 @@
 #include "lists.h"
 
 /**
+ * _strlen - entry point
+ * @s: string to find length
+ * Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
  * add_node - Short description
  * @head: constant number n to be summed
  * @str: xd
@@ -19,7 +38,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = strlen(new_node->str);
+	new_node->len = _strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
