04006f6ed419b807de5b481d6fb750eb5e69e221
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-19 02:06:08 +0000

    advanced 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before - Short description
+ * constant number n to be summed
+ * separator xd
+ * Return: Sum of undefined quantity of numbers
+ * Description: Sum numbers
+ */
+
+void print_before()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
