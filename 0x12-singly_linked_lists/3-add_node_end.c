3b9215aeef50d8966d81d7a9acde0024953cc0bf
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-19 01:02:48 +0000

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -42,12 +42,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 	if (!(*head))
-        {
-                *head = new_node;
-                return (new_node);
-        }
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	while (last->next)
-                last = last->next;
+		last = last->next;
 	last->next = new_node;
 	return (new_node);
 }
