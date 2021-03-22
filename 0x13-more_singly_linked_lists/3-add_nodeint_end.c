172a35e4263ee9fdd579757703aebe71deacac1a
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-22 13:24:15 +0000

    task 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Short description
+ * @head: constant number n to be summed
+ * @str: xd
+ * Return: Sum of undefined quantity of numbers
+ * Description: Sum numbers
+ */
+
+listint_t *add_nodeint_end(listint_t **head, int n)
+{
+	listint_t *new_node, *last;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	last = *head;
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!(*head))
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (last->next)
+		last = last->next;
+	last->next = new_node;
+	return (new_node);
+}
