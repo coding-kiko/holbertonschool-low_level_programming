232ec0b6cc54b159b7d630e1825571e65a797d3c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-22 20:05:22 +0000

    task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - Short description
+ * @head: constant number n to be summed
+ * @index: index node xd
+ * @n: Sum of undefined quantity of numbers
+ * Description: Sum numbers
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t temp;
+
+	
+}
