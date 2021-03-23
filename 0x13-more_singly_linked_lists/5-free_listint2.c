a1c2b6dd9c14cc40092afdd869e996f418df6e93
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-23 16:55:43 +0000

    updated task 5 border case

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,15 +14,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *aux;
 
-	if (*head)
+	if (!*head)
+		return;
+	aux = *head;
+	while (aux)
 	{
-		aux = *head;
-		while (aux)
-		{
-			temp = aux;
-			aux = aux->next;
-			free(temp);
-		}
-		*head = NULL;
+		temp = aux;
+		aux = aux->next;
+		free(temp);
 	}
+	*head = NULL;
 }
