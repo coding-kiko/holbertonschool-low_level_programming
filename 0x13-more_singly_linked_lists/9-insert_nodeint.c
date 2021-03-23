d31565717711b10b56d70ef421ce18817483663e
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-23 19:12:33 +0000

    task 9 updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
-	if (!*head)
+	if (!head)
 	{
 		*head = new_node;
 		return (NULL);
