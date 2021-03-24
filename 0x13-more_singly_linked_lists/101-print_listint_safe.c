a929bd2965c6fbb23b0791348dbda4656e8b7c58
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-24 00:14:28 +0000

    free points

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - Short description
+ * @head: constant number n to be summed
+ * separator xd
+ * Return: Sum of undefined quantity of numbers
+ * Description: Sum numbers
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head;
+	unsigned int count = 0;
+
+	return (count);
+}
