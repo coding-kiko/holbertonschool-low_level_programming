492911ba2084f0f1919d3f8a869ac018f393bd35
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-25 16:35:34 +0000

    header file updated

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,10 +2,6 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
-void rev_string(char *s);
-int _strlen(char *s);
-int _atoi(char s);
-int _pow_recursion(int x, int y);
 unsigned int binary_to_uint(const char *b);
 
 #endif
