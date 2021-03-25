7634dabfd31da1f5fbbdfe1c5e6136749bca86ae
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-25 22:06:17 +0000

    task 2 failed successfully

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	if (((n >> index)&0x01) == 0)
+	if (((n >> index) & 1) == 0)
 		return (0);
 	return (1);
 }
