a86907ec32c652255a156331506c47e4af36c9e6
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-29 20:28:10 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+
+/**
+ * _strlen - entry point
+ * @s: string to find length
+ * Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+		return (0);
+	while (*s != 0)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * create_file - Entry point
+ * @filename: string to modify
+ * @text_content: thiago silva
+ * Return: pointer to memory area s
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	char *buff;
+	ssize_t written;
+
+	if (!filename)
+		return (-1);
+	len = _strlen(text_content);
+	buff = malloc(sizeof(char) * (len + 1));
+	if (!buff)
+		return (0);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		free(buff);
+		return (-1);
+	}
+	written = write(fd, text_content, len);
+	if (written == -1)
+	{
+		free(buff);
+		return (-1);
+	}
+	close(fd);
+	free(buff);
+	return (1);
+}
