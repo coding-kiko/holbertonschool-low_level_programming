ba0ce51399e624df54361538f8658f1dd1423b61
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-29 19:15:37 +0000

    updated return value on failure

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,13 +20,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	count = read(fd, buff, letters);
 	if (count != -1)
 	{
 		written = write(STDOUT_FILENO, buff, letters);
 		if (written == -1)
+		{
+			free(buff);
 			return (0);
+		}
 	}
 	free(buff);
 	close(fd);
