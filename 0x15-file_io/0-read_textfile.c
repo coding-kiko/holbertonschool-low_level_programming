16c6c00bd9895b8b6dbf3eff182edf368b659128
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-29 19:12:30 +0000

    updated return value on failure

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff;
 
 	if (!filename)
-                return (0);
+		return (0);
 	buff = malloc(sizeof(char) * letters);
 	if (!buff)
 		return (0);
