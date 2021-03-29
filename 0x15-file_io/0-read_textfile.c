2f044914eb7f779dd6bb570b23424c7661fe981c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-29 18:53:56 +0000

    readme, task 0 and header

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - Entry point
+ * @filename: string to modify
+ * @letters: thiago silva
+ * Return: pointer to memory area s
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = 0;
+	ssize_t count = 0, writ = 0;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (!filename || !buff)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	count = read(fd, buff, letters);
+	if (count != -1)
+	{
+		writ = write(STDOUT_FILENO, buff, letters);
+		if (writ == -1)
+			return (-1);
+	}
+	free(buff);
+	close(fd);
+	return (count);
+}
