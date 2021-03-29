97556190692be9279edcca1b89208711e1eb79ba
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-29 18:56:25 +0000

    readme, task 0 and header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,11 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t count = 0, writ = 0;
 	char *buff = malloc(sizeof(char) * letters);
 
-	if (!filename || !buff)
+	if (!buff)
+		return (0);
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 	count = read(fd, buff, letters);
 	if (count != -1)
 	{
