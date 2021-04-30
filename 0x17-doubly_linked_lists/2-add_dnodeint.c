fb925a729f5b42b2868bef5b00477d2d6f90bc72
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-04-30 17:51:50 +0000

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Entry point
+ * @n: new node data
+ * @head: head node
+ * Return: adress on succes, null on failure
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+  dlistint_t *new = NULL;
+
+  new = malloc(sizeof(dlistint_t));
+  if (new == NULL)
+    return(NULL);
+  if (!head)
+    return(NULL);
+  new->n = n;
+  new->next = *head;
+  new->prev = NULL;
+  *head = new;
+  return(new);
+}
