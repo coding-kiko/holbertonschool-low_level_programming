0aab311e71a96f8a9f996c5efe6cf8bff9517dae
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-04-30 15:54:36 +0000

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Entry point
+ * @h: node to begin with
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+  const dlistint_t *aux = NULL;
+  size_t c = 0;
+
+  if (h == NULL)
+    return(c);
+  aux = h;
+  while (aux->prev != NULL)
+    {
+      aux = aux->prev;
+    }
+  while (aux != NULL)
+    {
+      aux = aux->next;
+      c++;
+    }
+  return(c);
+}
