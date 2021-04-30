2a6b9e8df981cc233348157ad3cc59f52b675659
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-04-30 20:10:28 +0000

    task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Short description
+ * @head: constant number n to be summed
+ * @index: index node xd
+ * Return: Sum of undefined quantity of numbers
+ * Description: Sum numbers
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+  dlistint_t *temp = head;
+  unsigned int i = 0;
+
+  while ((i < index) && temp)
+    {
+      temp = temp->next;
+      i++;
+    }
+  return (temp);
+}
