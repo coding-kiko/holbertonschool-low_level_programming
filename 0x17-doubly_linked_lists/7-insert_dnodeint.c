d8534bbacfd9afc1bf48496060294b2126752a60
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-01 19:31:33 +0000

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,39 +8,43 @@
  * Return: Sum numbers
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-  dlistint_t *temp, *new_node;
-  unsigned int i = 0;
+	dlistint_t *temp, *new;
+	unsigned int i = 0;
 
-  new_node = malloc(sizeof(dlistint_t));
-  if (!new_node)
-    return (NULL);
-  new_node->n = n;
-  new_node->next = NULL;
-  if (!head)
-    {
-      *head = new_node;
-      return (NULL);
-    }
-  if (idx == 0)
-    {
-      new_node->next = *head;
-      *head = new_node;
-      return (new_node);
-    }
-  temp = *head;
-  while ((i < (idx - 1)) && temp)
-    {
-      temp = temp->next;
-      i++;
-    }
-  if (temp)
-    {
-      new_node->next = temp->next;
-      temp->next = new_node;
-      return (new_node);
-    }
-  else
-    return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (!h && idx == 0)
+	{
+		*h = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+	if (idx == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	temp = *h;
+	while ((i < (idx - 1)) && temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp)
+	{
+		new->next = temp->next;
+		new->prev = temp;
+		temp->next->prev = new;
+		temp->next = new;
+		return (new);
+	}
+	else
+		return (NULL);
 }
