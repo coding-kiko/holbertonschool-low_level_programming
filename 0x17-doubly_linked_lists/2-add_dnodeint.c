7a38e1dfaf45159fe2b15113a8f216ffb26e6156
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-01 16:41:29 +0000

    task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (!head)
-		return (NULL);
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
