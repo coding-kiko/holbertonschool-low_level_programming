2bf3d3ce4863eead4d765f3864333145cef38833
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-01 21:19:32 +0000

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,26 +10,29 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-  dlistint_t *temp = *head, *after_removed;
-  unsigned int i = 0;
+	dlistint_t *temp = *head, *after_removed;
+	unsigned int i = 0;
 
-  if (!*head)
-    return (-1);
-  if (index == 0)
-    {
-      *head = temp->next;
-      free(temp);
-      return (1);
-    }
-  while ((i < (index - 1)) && temp)
-    {
-      temp = temp->next;
-      i++;
-    }
-  if (!temp || !(temp->next))
-    return (-1);
-  after_removed = temp->next->next;
-  free(temp->next);
-  temp->next = after_removed;
-  return (1);
+	if (!*head)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while ((i < (index - 1)) && temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (!temp || !(temp->next))
+		return (-1);
+	after_removed = temp->next->next;
+	free(temp->next);
+	temp->next = after_removed;
+	after_removed->prev = temp;
+	return (1);
 }
