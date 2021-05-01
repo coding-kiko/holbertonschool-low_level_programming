881497cee1b6c1d385e1edeb16dd5988700fb655
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-01 16:07:44 +0000

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,21 +8,21 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-  const dlistint_t *aux = NULL;
-  size_t c = 0;
+	const dlistint_t *aux = NULL;
+	size_t c = 0;
 
-  if (h == NULL)
-    return(c);
-  aux = h;
-  while (aux->prev != NULL)
-    {
-      aux = aux->prev;
-    }
-  while (aux != NULL)
-    {
-      printf("%d\n", aux->n);
-      aux = aux->next;
-      c++;
-    }
-  return(c);
+	if (h == NULL)
+		return (c);
+	aux = h;
+	while (aux->prev != NULL)
+	{
+		aux = aux->prev;
+	}
+	while (aux != NULL)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+		c++;
+	}
+	return (c);
 }
