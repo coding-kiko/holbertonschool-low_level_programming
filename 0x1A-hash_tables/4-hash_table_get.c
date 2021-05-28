a91b05d9295e85e3afbc337413aac794ba0632ee
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-28 18:25:42 +0000

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - creates hash table with size @size
+ *
+ * @ht: The size of the array
+ * @key: Each cell of this array is a pointer to the first node of a linke
+ * Return: value associated with key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx, size;
+	hash_node_t *temp;
+
+	if (!ht || !key)
+		return (NULL);
+	size = ht->size;
+	idx = hash_djb2((const unsigned char *)key) % size;
+	temp = ht->array[idx];
+	while (temp)
+	{
+		if (strcmp((temp->key), ((char *)key)) == 0)
+			return (temp->value);
+	}
+	return (NULL);
+}
