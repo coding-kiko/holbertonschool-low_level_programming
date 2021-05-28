3b4a82a9eb973ad4664ee40088e80d5c050852d9
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-28 03:13:04 +0000

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,20 +6,29 @@
  * @size: The size of the array
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
+ * Return: pointer to the table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 	unsigned long int i;
+	hash_node_t **nodos;
 
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
-	if (!(table->array))
-		return NULL;
+	nodos = malloc(sizeof(hash_node_t **) * size);
+	if (!nodos)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->array = nodos;
 	for (i = 0; i < size; i++)
 	{
-		table->array[i] = NULL;
+		nodos[i] = NULL;
 	}
 	return (table);
 }
