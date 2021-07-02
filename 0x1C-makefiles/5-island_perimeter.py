283de34a2a7bf6dceabd7e25427ee53a2bbb3336
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-07-02 22:11:58 +0000

    technical interview

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,4 +4,25 @@
 
 def island_perimeter(grid):
     '''returns the perimeter of the island described in grid (matrix)'''
-    pass
+    if len(grid) == 0:
+        return 0
+    width = len(grid[0])
+    height = len(grid)
+    count = 0
+
+    for y in range(height):
+        for x in range(width):
+            if grid[y][x] == 1:  # current element
+                # ask if element above exists, then ask which element it is
+                if y != 0 and grid[y - 1][x] == 0:
+                    count += 1
+                # ask if element below exists, then ask which element it is
+                if y != (height - 1) and grid[y + 1][x] == 0:
+                    count += 1
+                # ask if element to left exists, then ask which element it is
+                if x != 0 and grid[y][x - 1] == 0:
+                    count += 1
+                # ask if element to right exists, then ask which element it is
+                if x != (width - 1) and grid[y][x + 1] == 0:
+                    count += 1
+    return count
