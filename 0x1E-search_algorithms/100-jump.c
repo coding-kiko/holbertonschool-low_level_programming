43e0207a268ecb9e590b12f201f397c849c1a47c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-31 15:33:51 -0300

    betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,16 +10,16 @@
  * @value: value to search for
  * Return: Always EXIT_SUCCESS
  */
- int jump_search(int *array, size_t size, int value)
- {
+int jump_search(int *array, size_t size, int value)
+{
 	size_t key = 0, i, s, prev;
 
 	if (!array)
-		return (-1); 
+		return (-1);
 	while (array[key] < value)
 	{
 		printf("Value checked array[%lu] = [%i]\n", key, array[key]);
-    	key += sqrt(size);
+		key += sqrt(size);
 		if (key > size)
 			return (-1);
 	}
