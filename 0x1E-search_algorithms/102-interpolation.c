8639b95ea8415365dad0af085e6decf09a7d255b
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-31 17:31:58 -0300

    task 102

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * interpolation_search - linear search for element in array
+ * @array: array to search in
+ * @size: size of array
+ * @value: value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t high = size - 1, low = 0;
+	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
+
+	if (!array)
+		return (-1);
+
+	for (; pos <= size; pos++)
+	{
+		printf("Value checked array[%lu] = [%i]\n", pos, array[pos]);
+		if (array[pos] == value)
+		{
+			return (pos);
+		}
+	}
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *a, int s, int f);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+
 #endif
