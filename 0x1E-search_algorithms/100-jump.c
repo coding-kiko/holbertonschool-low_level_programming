b418c3d59ac501ea650bd0209f7f8af9afae5f9d
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-31 15:30:59 -0300

    task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - linear search for element in array
+ * @array: array to search in
+ * @size: size of array
+ * @value: value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+ int jump_search(int *array, size_t size, int value)
+ {
+	size_t key = 0, i, s, prev;
+
+	if (!array)
+		return (-1); 
+	while (array[key] < value)
+	{
+		printf("Value checked array[%lu] = [%i]\n", key, array[key]);
+    	key += sqrt(size);
+		if (key > size)
+			return (-1);
+	}
+	s = sqrt(size);
+	prev = key - s;
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, key);
+	for (i = prev; i <= size; i++)
+	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *a, int s, int f);
+int jump_search(int *array, size_t size, int value);
 #endif
