14c70a92852914f8f0da0b119f1ee824ee743ada
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-31 18:37:22 -0300

    edge case

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,13 +11,25 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t high = size - 1, low = 0;
-	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) *
-				(value - array[low]));
+	size_t high = size - 1, low = 0, pos;
 
 	if (!array)
 		return (-1);
-
+	if (array[high] == array[low])
+	{
+		if (array[low] == value)
+		{
+			printf("Value checked array[%lu] = [%i]\n", low, array[low]);
+			return (low);
+		}
+		else
+		{
+			/*printf("Value checked array[%lu] is out of range\n", low);*/
+			return (-1);
+		}
+	}
+	pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
 	for (; pos <= size; pos++)
 	{
 		printf("Value checked array[%lu] = [%i]\n", pos, array[pos]);
