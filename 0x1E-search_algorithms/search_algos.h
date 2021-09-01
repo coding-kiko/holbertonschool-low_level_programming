7ac0991714a7d151554bfbfba16afdcfb3d9a682
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-01 12:23:37 -0300

    headerrr

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,10 @@
 #define SEARCH_ALGOS_H
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value, size_t l);
 void print_array(int *a, int s, int f);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
