878777383860c05d4b594b5da1bb0f429c294932
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-01 15:13:33 -0300

    betty-header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,9 +20,9 @@ int exponential_search(int *array, size_t size, int value);
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *create_list(int *array, size_t size);
